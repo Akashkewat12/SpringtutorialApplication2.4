f3287e3cfe216fe31491beb95aa1509d70e073a5
Akashkewat12 <akashbind.bca.15@gmail.com>
2025-01-17 04:25:57 +0530

    first commit

---
+++ src/test/java/com/spring/Springtutorial/SpringtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Springtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
